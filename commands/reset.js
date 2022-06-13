f04a64304dec95864b72dbd01c10cf01b19c5808
TheRegressor <105577064+TheRegressor@users.noreply.github.com>
2022-06-12 20:09:17 -0700

    add some more info, grammar fix?

--- commands/reset.js
+++ commands/reset.js
@@ -3,17 +3,17 @@ const {Users} = require('../db')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('reset')
-    .setDescription('Reset ALL USERS'),
+    .setDescription('Reset ALL USERS proxy generations.'),
     ownerOnly: true,
     async execute(interaction){
         try{
             Users.destroy({
                 where: {}
             })    
-            return interaction.reply('All users have been reset.')
+            return interaction.reply('All users proxy generations have been reset.')
         }
         catch(err){
-            interaction.reply('Something went wrong. Check the log!')
+            interaction.reply('Something went wrong. Please check the log!')
             console.log(client.colors.error(err))
         }
         
